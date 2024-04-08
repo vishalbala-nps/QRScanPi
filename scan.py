f5bee16bc750635026fe2f51e250d5b19f39beca
Vishal Balasubramanian <vishal.bala.nps@gmail.com>
2024-04-08 14:04:31 +0530

    Adding options between pyzbar and opencv's qr code scanner

--- scan.py
+++ scan.py
@@ -1,7 +1,15 @@
 import cv2 
 import sys
 import json
-
+try:
+	usez = sys.argv[3]
+	if usez == "pyzbar" or usez == "zbar":
+		zbar = True
+		from pyzbar.pyzbar import decode
+	else:
+		zbar = False
+except:
+	zbar = False
 if len(sys.argv) == 0:
 	print("No hintfile specified! Exiting..")
 	exit(1)
@@ -16,12 +24,27 @@ else:
 	except:
 		print("Camera not specified/not found. Attempting to use default camera")
 		cap = cv2.VideoCapture(0)
+	cap.set(3,384)
+	cap.set(4,288)
+	if zbar:
+		print("Using pyzbar")
+	else:
+		print("Using OpenCV QRCodeDetector")
 	detector = cv2.QRCodeDetector()
 	print("Starting scanner..")
+	prv = None
 	while True: 
 		_, img = cap.read()
-		data, bbox, _ = detector.detectAndDecode(img)
-		if data:
+		if zbar:
+			if len(decode(img)) != 0:
+				data = decode(img)[0].data.decode("utf-8")
+			else:
+				data = ""
+				pass
+		else:
+			data, bbox, _ = detector.detectAndDecode(img)
+		if data and prv != data:
+			prv = data
 			print("Scanned data:",data)
 			for i in f:
 				if i["id"] == data:
